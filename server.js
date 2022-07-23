2202fff73f100cbfb0a78ec21059c72898b1172b
aldoalejandro117 <74940144+aldoalejandro117@users.noreply.github.com>
2022-07-23 12:26:05 -0400

    test logginh

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const {animals} = require('./data/animals');
-
+console.log (animals);
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Save the animalsArray as filteredResults here:
